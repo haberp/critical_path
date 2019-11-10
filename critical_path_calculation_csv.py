26cdeb23e43058ae3470ed8c01f0472333b43e58
haberp <56176737+haberp@users.noreply.github.com>
2019-11-10 10:47:30 +0100

    Update critical_path_calculation_csv.py

--- critical_path_calculation_csv.py
+++ critical_path_calculation_csv.py
@@ -1,17 +1,17 @@
 import csv
 import re
 import datetime
+from urllib.request import urlopen
 import lxml.etree as ET
+
 bpmn_file_name = 'C:/Users/vajda/Downloads/EDM - Consumption data collection at Nordics -at- 2019-11-06.bpmn'
 tree = ET.parse(bpmn_file_name)
 root = tree.getroot()
 
-original_file_name = re.findall('([0-9a-zA-Z- ]+)\.', bpmn_file_name)
-
-csvFileName = "{}-on-{}-at-{}.csv".format(original_file_name[0], datetime.date.today(), datetime.datetime.now().strftime('%H-%M-%S'))
+#process = tree.xpath('//bpmn:process', namespaces = root.nsmap)
+#print(root.find('bpmn:process', root.nsmap))
 
 delimiter_char = ";"
-#print(root.find('bpmn:process', root.nsmap))
 
 parent_map = {c.tag:p.tag for p in root.iter( ) for c in p}
 
@@ -31,16 +31,12 @@ for task in root.iter('{http://www.omg.org/spec/BPMN/20100524/DI}BPMNEdge'):
         task_id = task.get('bpmnElement')
         color_dict[task_id] = task_color
 
-#print(color_dict)
-
 elements_under_process = []
 
 #collecting elements under the process section
 for key, value in parent_map.items():
     if value == '{http://www.omg.org/spec/BPMN/20100524/MODEL}process':
         elements_under_process.append(key)
-
-#print(elements_under_process)
 
 process_element_ids = {}
 
@@ -49,15 +45,11 @@ for element in elements_under_process:
     for _ in root.iter(element):
         process_element_ids[_.get('id')] = _.get('name')
 
-#print(process_element_ids)
-
 # if process element id is in the color dictionary selects the the id and name of the process element
 list_of_elements_on_critical_path = {}
 for task_id, name in process_element_ids.items():
     if task_id in color_dict.keys():
         list_of_elements_on_critical_path[task_id] = [name]
-
-#print(list_of_elements_on_critical_path)
 
 #getting duration and task id of process elements
 list_of_elements_with_duration = {}
@@ -70,8 +62,6 @@ for element in elements_under_process:
         else:
             task_id = task.get('id')
             list_of_elements_with_duration[task_id] = duration
-
-#print(list_of_elements_with_duration)
 
 #extending the process element id and name with duration
 for key, duration in list_of_elements_with_duration.items():
@@ -87,8 +77,19 @@ for key, value in list_of_elements_on_critical_path.items():
 for key in deletable_keys:    
     del list_of_elements_on_critical_path[key]
 
+#Creating file name for csv creation
+original_file_name = re.findall('([0-9a-zA-Z- ]+)\.', bpmn_file_name)
+csvFileName = "{}-on-{}-at-{}.csv".format(original_file_name[0], datetime.date.today(), datetime.datetime.now().strftime('%H-%M-%S'))
+
+#Adding key type to the list
+for key, value in  list_of_elements_on_critical_path.items():
+    split = key.split('_')
+    list_of_elements_on_critical_path[key].append(split[0])
+
 #writing element id, name and duration to csv
+
 with open (csvFileName, mode='w', newline='\n') as f:
     writer = csv.writer(f, delimiter = delimiter_char, quotechar = '"', quoting=csv.QUOTE_MINIMAL)
     for key in  list_of_elements_on_critical_path.keys():
-        writer.writerow([key, list_of_elements_on_critical_path[key][1].encode('unicode-escape').decode(), list_of_elements_on_critical_path[key][0].encode('unicode-escape').decode()])
+        writer.writerow([list_of_elements_on_critical_path[key][1].encode('unicode-escape').decode(), list_of_elements_on_critical_path[key][2].encode('unicode-escape').decode(), key, list_of_elements_on_critical_path[key][0].encode('unicode-escape').decode()])
+    print('CSV file created')
